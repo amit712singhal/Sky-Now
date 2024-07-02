55290995cbe3caac6b1a31264b73107a4376a26c
Amit Singhal <rakshit.singhal712@gmail.com>
2024-07-02 17:49:16 +0530

    Add Routes for default, current & searched Location

--- js/route.js
+++ js/route.js
@@ -4,3 +4,52 @@
  * @copyright amit712singhal 2024 All rights reserved
  * @author amit712singhal <rakshit.singhal@gmail.com>
  */
+
+`use strict`;
+
+import { updateWeather, error404 } from "./app.js";
+
+const defaultLocation = "#/weather?lat=28.6667&lon=77.2167"; //Delhi, India
+
+const currentLocation = function () {
+  window.navigator.geolocation.getCurrentPosition(
+    (res) => {
+      const { latitude, longitude } = res.coords;
+      updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+    },
+    (err) => {
+      window.location.hash = defaultLocation;
+    }
+  );
+};
+
+/**
+ *
+ * @param {string} query Searched Query
+ */
+const searchedLocation = (query) => updateWeather(...query.split("&"));
+//updateWeather("lat=28.6667", "lon=77.2167")
+
+const routes = new Map([
+  ["/current-location", currentLocation],
+  ["weather", searchedLocation],
+]);
+
+const checkHash = function () {
+  const requestURL = window.location.hash.slice(1);
+  const [route, query] = requestURL.includes
+    ? requestURL.split("?")
+    : [requestURL];
+
+  routes.get(route) ? routes.get(route)(query) : error404();
+};
+
+window.addEventListener("hashchange", checkHash);
+
+window.addEventListener("load", function () {
+  if (!window.location.hash) {
+    window.location.hash = "#/current-location";
+  } else {
+    checkHash();
+  }
+});
