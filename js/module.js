14848a61f34f191336933ad3901d49fe1ade274f
Amit Singhal <rakshit.singhal712@gmail.com>
2024-07-02 17:15:09 +0530

    Add Real-Time Data for Updation

--- js/module.js
+++ js/module.js
@@ -4,3 +4,95 @@
  * @copyright amit712singhal 2024 All rights reserved
  * @author amit712singhal <rakshit.singhal@gmail.com>
  */
+
+"use strict";
+
+export const weekDayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+/**
+ * @param {number} dateUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns  {string} Date String {format: "Tuesday 2, Jul"}
+ */
+
+export const getDate = function (dateUnix, timezone) {
+  const date = new Date((dateUnix + timezone) * 1000);
+  const weekDayName = weekDayNames[date.getUTCDay()];
+  const monthName = monthNames[date.getUTCMonth()];
+  return `${weekDayName} ${date.getUTCDate()}, ${monthName}`;
+};
+
+/**
+ * @param {number} timeUnix Unix time in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Time String {format: "HH AM/PM"}
+ */
+
+export const getTime = function (timeUnix, timezone) {
+  const date = new Date((timeUnix, timezone) * 1000);
+  const hours = date.getUTCHours();
+  const period = hours >= 12 ? "PM" : "AM";
+  return `${hours % 12 || 12} ${period}`;
+};
+
+/**
+ * @param {number} mps meter/seconds
+ * @returns {number} kilometer/hours
+ */
+
+export const mpsTOkmh = (mps) => {
+  const mph = mps * 3600;
+  return mph / 1000;
+};
+
+export const aqiText = {
+  1: {
+    level: "Good",
+    message:
+      "Air quality is considered satisfactory, & air pollution possess little or no risk.",
+  },
+
+  2: {
+    level: "Fair",
+    message:
+      "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.",
+  },
+  3: {
+    level: "Moderate",
+    message:
+      "Members of sensitive groups may experience health effects. The general public is not likely to be affected.",
+  },
+  4: {
+    level: "Poor",
+    message:
+      "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.",
+  },
+  5: {
+    level: "Very Poor",
+    message:
+      "Health warnings of emergency conditions. The entire population is more likely to be affected.",
+  },
+};
