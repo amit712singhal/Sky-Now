ac6502f697b560113ea04ed7282933818674630a
Amit Singhal <rakshit.singhal712@gmail.com>
2024-07-03 10:30:52 +0530

    Add Location Search

--- js/app.js
+++ js/app.js
@@ -3,3 +3,78 @@
  * @copyright amit712singhal 2024 All rights reserved
  * @author amit712singhal <rakshit.singhal@gmail.com>
  */
+
+`use strict`;
+
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
+
+/**
+ *  Add event listener on multiple elements
+ * @param {NodeList} elements Elements node array
+ * @param {string} eventType Event Type e.g.; "click", "mouseover"
+ * @param {Function} callback Callback function
+ */
+const addEventListener = function (elements, eventType, callback) {
+  for (const element of elements) element.addEventListener(eventType, callback);
+};
+
+/**
+ * Toggle Search in mobile devices
+ */
+const searchView = document.querySelector("[data-search-view]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventListener(searchTogglers, "click", toggleSearch);
+
+/**
+ * SEARCH INTEGRATION
+ */
+
+const searchField = document.querySelector("[data-search-field]");
+const searchResult = document.querySelector("[data-search-result]");
+
+let searchTimeout = null;
+const searchTimeoutDuration = 500;
+
+searchField.addEventListener("input", function () {
+  searchTimeout ?? clearTimeout(searchTimeout);
+  if (!searchField.value) {
+    searchResult.classList.remove("active");
+    searchResult.innerHTML = "";
+    searchField.classList.remove("searching");
+  } else {
+    searchField.classList.add("searching");
+  }
+
+  if (searchField.value) {
+    searchTimeout = setTimeout(() => {
+      fetchData(url.geo(searchField.value), function (locations) {
+        searchField.classList.remove("searching");
+        searchResult.classList.add("active");
+        searchResult.innerHTML = `
+          <ul class="view-list" data-search-list>
+          </ul>
+        `;
+
+        const /** {NodeList} | [] */ items = [];
+        for (const { name, lat, lon, country, state } of locations) {
+          const searchItem = document.createElement("li");
+          searchItem.classList.add("view-item");
+          searchItem.innerHTML = `
+            <span class="m-icon">location_on</span>
+            <div>
+              <p class="item-title">${name}</p>
+              <p class="label-2 item-subtitle">${state || ""} ${country}</p>
+            </div>
+            <a href="#/weather?lat=${lat}&lon=${lon}" class="item-link has-state" aria-label="${name} weather" data-search-toggler></a>
+          `;
+
+          searchResult.querySelector("[data-search-list]").appendChild(searchItem);
+          items.push(searchItem.querySelector("[data-search-toggler]"))
+        }
+      });
+    }, searchTimeoutDuration);
+  }
+});
